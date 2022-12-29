611a7317059f22a32249d65d7d6c449b8ead4648
KevinWu <kevinwu200099@gmail.com>
2022-12-29 17:16:52 +0800

    correct version again

--- frontend/src/container/Login.js
+++ frontend/src/container/Login.js
@@ -1,11 +1,9 @@
 import {Input, Button} from 'antd';
-import styled from 'styled-components'
 import { UserOutlined } from "@ant-design/icons";
 import { useState , useRef } from "react";
 import {useHooks} from './hooks/Hooks'
 import Titlebar from './Titlebar'
 import { useNavigate } from 'react-router-dom';
-
 
 function Login() {
 	const [name, setName] = useState('');
@@ -23,34 +21,6 @@ function Login() {
 
 	return (
 		<>
-		<Input
-			size="large"
-			style={{ width: 300, margin: 10 }}
-			placeholder="Enter your name"
-			prefix={<UserOutlined />}
-			value={name}
-			onChange={(e) => setName(e.target.value)}
-			onKeyDown={(e) => {
-				if (e.key === 'Enter') {
-					bodyRef.current.focus()
-				}
-			}}
-		/>
-		<div/>
-		<Input.Search
-		ref={bodyRef}
-		size="large"
-		style={{ width: 300, margin: 10 }}
-		prefix={<UserOutlined />}
-		placeholder="Enter your password"
-		value={appear_password(password)}
-		onChange={(e) => setPassword(e.target.value)}
-		enterButton="Sign In"
-		onSearch={()=>{setLogin(true); setCurrentpage('Search');}}
-		/>
-		<div/>
-		<Button onClick={()=>{setCurrentpage('register')}}>Click here to register</Button>
-		
 			<Titlebar/>
 			<Input
 				size="large"
--- frontend/src/container/Search.js
+++ frontend/src/container/Search.js
@@ -1,21 +1,8 @@
 import { Input, Menu } from 'antd';
-import styled from 'styled-components'
-import { ShopOutlined, CoffeeOutlined} from "@ant-design/icons";
+import { ShopOutlined } from "@ant-design/icons";
 import { useState , useRef, useEffect } from "react";
 import {useHooks} from './hooks/Hooks'
 import TitlebarSearch from './TitlebarSearch'
-
-const SearchBackground = styled.div`
-	display : flex;
-	justify-content : center;
-`
-
-const InputSearch = styled(Input.Search)`
-	position : absolute;
-	width : 50%;
-	top : 20%;
-`
-
 
 function Search() {
 	return(
