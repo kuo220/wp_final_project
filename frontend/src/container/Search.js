8c9622850bc1115a9bb1c0d1b102aa23dc6c30cd
kuo220 <k0978337296@gmail.com>
2022-12-29 18:36:18 +0800

    update search.js

--- frontend/src/container/Search.js
+++ frontend/src/container/Search.js
@@ -1,19 +1,41 @@
 import { Input, Menu } from 'antd';
+import styled from 'styled-components'
 import { ShopOutlined } from "@ant-design/icons";
 import { useState , useRef, useEffect } from "react";
 import {useHooks} from './hooks/Hooks'
 import TitlebarSearch from './TitlebarSearch'
 
+const SearchBackground = styled.div`
+	display : flex;
+	justify-content : center;
+`
+
+const StyledSearch = styled(Input.Search)`
+	position : absolute;
+	display : flex;
+	width : 50%;
+	top : 20%;
+	justify-content : center;
+`
+
+const Background = {
+	background : 'gray',
+
+}
+
 function Search() {
 	return(
-    <>
+    <div style={{Background}}>
 		<TitlebarSearch/>
-		<Input.Search
-			size="large"
-			placeholder="Search for café"
-			prefix={<ShopOutlined />}
-		/>
-    </>
+		<SearchBackground>
+			<StyledSearch
+				size="large"
+				placeholder="Search for café"
+				prefix={<ShopOutlined />}
+			/>
+		</SearchBackground>
+	</div>
+		
   )
 }
 
