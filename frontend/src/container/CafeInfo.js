b0833963292420f400551a07a5ff9fe9f8fea318
kuo220 <k0978337296@gmail.com>
2023-01-01 21:51:45 +0800

    little update

--- frontend/src/container/CafeInfo.js
+++ frontend/src/container/CafeInfo.js
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom'
 
-function Cafe() {
+function CafeInfo() {
     const { id } = useParams()
 	return(
         <h1>
@@ -9,4 +9,4 @@ function Cafe() {
     )
 }
 
-export default Cafe;
+export default CafeInfo;
