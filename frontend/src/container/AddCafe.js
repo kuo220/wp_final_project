dadf998e2e213019885a3ea65adba377f3bfec1f
kuo220 <k0978337296@gmail.com>
2023-01-03 03:10:31 +0800

    update AddCafe form

--- frontend/src/container/AddCafe.js
+++ frontend/src/container/AddCafe.js
@@ -14,10 +14,9 @@ import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AddressForm from '../component/AddressForm';
 import BusinessHourForm from '../component/BusinessHourForm';
-import Review from '../component/Review';
 
 
-const steps = ['Basic Info', 'Business hours', 'Complete'];
+const steps = ['Basic Info', 'Business hours'];
 
 function getStepContent(step) {
 	switch (step) {
@@ -25,8 +24,6 @@ function getStepContent(step) {
 			return <AddressForm />;
 		case 1:
 			return <BusinessHourForm />;
-		case 2:
-			return <Review />;
 		default:
 			throw new Error('Unknown step');
 	}
@@ -57,56 +54,57 @@ function AddCafe() {
 					borderBottom: (t) => `1px solid ${t.palette.divider}`,
 				}}
 			>
-			<Toolbar>
-				<Typography variant="h6" color="inherit" noWrap>
-					Add New Café
-				</Typography>
-			</Toolbar>
+				<Toolbar>
+					<Typography variant="h6" color="inherit" noWrap>
+						Add New Café
+					</Typography>
+				</Toolbar>
 			</AppBar>
 			<Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
 				<Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-				<Typography component="h1" variant="h4" align="center">
-					Café Info
-				</Typography>
-				<Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
-					{steps.map((label) => (
-					<Step key={label}>
-						<StepLabel>{label}</StepLabel>
-					</Step>
-					))}
-				</Stepper>
+					<Typography component="h1" variant="h4" align="center">
+						Café Info
+					</Typography>
 
-				{activeStep === steps.length ? (
-				<React.Fragment>
-					<Typography variant="h5" gutterBottom>
-						Thank you for your order.
-					</Typography>
-					<Typography variant="subtitle1">
-						Your order number is #2001539. We have emailed your order
-						confirmation, and will send you an update when your order has
-						shipped.
-					</Typography>
-					</React.Fragment>
-				) : (
+					<Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+						{steps.map((label) => (
+						<Step key={label}>
+							<StepLabel>{label}</StepLabel>
+						</Step>
+						))}
+					</Stepper>
+
+					{activeStep === steps.length ? (
 					<React.Fragment>
-					{getStepContent(activeStep)}
-					<Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-						{activeStep !== 0 && (
-						<Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
-							Back
-						</Button>
-						)}
+						<Typography variant="h5" gutterBottom>
+							Thank you for your Café Review.
+						</Typography>
+						{/* <Typography variant="subtitle1">
+							Your order number is #2001539. We have emailed your order
+							confirmation, and will send you an update when your order has
+							shipped.
+						</Typography> */}
+					</React.Fragment>
+					) : (
+						<React.Fragment>
+							{getStepContent(activeStep)}
+							<Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+								{activeStep !== 0 && (
+								<Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+									Back
+								</Button>
+								)}
 
-						<Button
-							variant="contained"
-							onClick={handleNext}
-							sx={{ mt: 3, ml: 1 }}
-						>
-							{activeStep === steps.length - 1 ? 'Place order' : 'Next'}
-						</Button>
-					</Box>
-				</React.Fragment>
-				)}
+								<Button
+									variant="contained"
+									onClick={handleNext}
+									sx={{ mt: 3, ml: 1 }}
+								>
+									{activeStep === steps.length - 1 ? 'Done' : 'Next'}
+								</Button>
+							</Box>
+						</React.Fragment>
+					)}
 				</Paper>
 			</Container>
 		</ThemeProvider>
