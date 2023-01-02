b023299c682342a6fcd2c41e9d93b84c515d61ca
kuo220 <k0978337296@gmail.com>
2023-01-02 23:35:26 +0800

    update Search

---
+++ frontend/src/container/AddCafe.js
@@ -0,0 +1,3 @@
+import { useState , useRef, useEffect } from "react";
+import styled from 'styled-component'
+import {useHooks} from './hooks/Hooks'
--- frontend/src/container/Search.js
+++ frontend/src/container/Search.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import styled from 'styled-components'
 import { Input, Button } from 'antd';
-import { ShopOutlined, PlusOutlined } from "@ant-design/icons";
+import { ShopOutlined, PlusOutlined, LogoutOutlined } from "@ant-design/icons";
 import AppBar from '@mui/material/AppBar';
 // import Button from '@mui/material/Button';
 import LocalCafeIcon from '@mui/icons-material/LocalCafe';
@@ -18,6 +18,8 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from 'react-router-dom';
+import {useHooks} from './hooks/Hooks'
 
 
 const SearchBox = styled(Input.Search)`
@@ -30,136 +32,142 @@ const SearchBox = styled(Input.Search)`
 `
 const AddButton = styled(Button)`
 	position : absolute;
-    background : #8A5A00;
+    // background : #8A5A00;
     top : 2.5%;
-    right : 5%
+    right : 10%
+`
+const LogoutButton = styled(Button)`
+	position : absolute;
+	top : 2.5%;
+	right : 5%
 `
 
 function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+	return (
+		<Typography variant="body2" color="text.secondary" align="center">
+			{'Copyright © '}
+			<Link color="inherit" href="https://mui.com/">
+			Your Website
+			</Link>{' '}
+			{new Date().getFullYear()}
+			{'.'}
+		</Typography>
+	);
 }
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const theme = createTheme();
 
-export default function Album() {
-  return (
-    <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <AppBar position="relative">
-        <Toolbar style={{backgroundColor:"#F8C471"}}>
-            <LocalCafeIcon sx={{ mr: 2 }} />
-            <Typography variant="h6" color="inherit" noWrap>
-                Café Finder
-            </Typography>
-            </Toolbar>
-        </AppBar>
-        <main>
-        {/* Hero unit */}
-        <Box
-          sx={{
-            bgcolor: 'background.paper',
-            pt: 8,
-            pb: 6,
-          }}
-        >
-            <Container maxWidth="sm">
-            {/* <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="text.primary"
-              gutterBottom
-            >
-              Album layout
-            </Typography> */}
-            <SearchBox
-                size="large"
-                placeholder="Search for café"
-                prefix={<ShopOutlined />}
-            />
-            <AddButton type='primary'>
-                <PlusOutlined/>
-            </AddButton>
-            
-            {/* <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Something short and leading about the collection below—its contents,
-              the creator, etc. Make it short and sweet, but not too short so folks
-              don&apos;t simply skip over it entirely.
-            </Typography> */}
-            <Stack
-                sx={{ pt: 4 }}
-                direction="row"
-                spacing={2}
-                justifyContent="center"
-            >
-                {/* <Button variant="contained">Main call to action</Button> */}
-                {/* <Button variant="outlined">Secondary action</Button> */}
-            </Stack>
-            </Container>
-        </Box>
-        <Container sx={{ py: 8 }} maxWidth="md">
-          {/* End hero unit */}
-            <Grid container spacing={4}>
-            {cards.map((card) => (
-                <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                    <CardMedia
-                        component="img"
-                        sx={{
-                        // 16:9
-                        pt: '56.25%',
-                        }}
-                        image="https://source.unsplash.com/random"
-                        alt="random"
-                    />
-                    <CardContent sx={{ flexGrow: 1 }}>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            Heading
-                        </Typography>
-                        <Typography>
-                            This is a media card. You can use this section to describe the
-                            content.
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small">View</Button>
-                        <Button size="small">Edit</Button>
-                    </CardActions>
-                </Card>
-              </Grid>
-            ))}
-            </Grid>
-        </Container>
-        </main>
-        {/* Footer */}
-        {/* <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-            <Typography variant="h6" align="center" gutterBottom>
-                Footer
-            </Typography>
-            <Typography
-                variant="subtitle1"
-                align="center"
-                color="text.secondary"
-                component="p"
-            >
-            Something here to give the footer a purpose!
-            </Typography>
-        <Copyright />
-        </Box> */}
-        {/* End footer */}
-    </ThemeProvider>
-  );
+function Search() {
+	const navigate = useNavigate();
+	const {login, setLogin} = useHooks();
+
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<AppBar position="relative">
+			<Toolbar style={{backgroundColor:"#8A5A00"}}>
+				<LocalCafeIcon sx={{ mr: 2 }} />
+				<Typography variant="h6" color="inherit" noWrap>
+					Café Finder
+				</Typography>
+				</Toolbar>
+			</AppBar>
+			<main>
+			{/* Hero unit */}
+			<Box
+			sx={{
+				bgcolor: 'background.paper',
+				pt: 8,
+				pb: 6,
+			}}
+			>
+				<Container maxWidth="sm">
+
+				<SearchBox
+					size="large"
+					placeholder="Search for café"
+					prefix={<ShopOutlined />}
+				/>
+
+				<AddButton type='default'>
+					<PlusOutlined/>
+				</AddButton>
+
+				<LogoutButton type='default'
+					onClick={() => {
+						setLogin(false);
+						navigate('/')
+					}}
+				>
+					<LogoutOutlined />
+				</LogoutButton>
+				
+				<Stack
+					sx={{ pt: 4 }}
+					direction="row"
+					spacing={2}
+					justifyContent="center"
+				>
+
+				</Stack>
+				</Container>
+			</Box>
+			<Container sx={{ py: 8 }} maxWidth="md">
+			{/* End hero unit */}
+				<Grid container spacing={4}>
+				{cards.map((card) => (
+					<Grid item key={card} xs={12} sm={6} md={4}>
+					<Card
+					sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+					>
+						<CardMedia
+							component="img"
+							sx={{
+							// 16:9
+							pt: '56.25%',
+							}}
+							image="https://source.unsplash.com/random"
+							alt="random"
+						/>
+						<CardContent sx={{ flexGrow: 1 }}>
+							<Typography gutterBottom variant="h5" component="h2">
+								Heading
+							</Typography>
+							<Typography>
+								This is a media card. You can use this section to describe the
+								content.
+							</Typography>
+						</CardContent>
+						<CardActions>
+							<Button size="small">View</Button>
+							<Button size="small">Edit</Button>
+						</CardActions>
+					</Card>
+				</Grid>
+				))}
+				</Grid>
+			</Container>
+			</main>
+			{/* Footer */}
+			{/* <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+				<Typography variant="h6" align="center" gutterBottom>
+					Footer
+				</Typography>
+				<Typography
+					variant="subtitle1"
+					align="center"
+					color="text.secondary"
+					component="p"
+				>
+				Something here to give the footer a purpose!
+				</Typography>
+			<Copyright />
+			</Box> */}
+			{/* End footer */}
+		</ThemeProvider>
+	);
 }
+
+export default Search;
