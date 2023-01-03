b0ad11fd05b14268eccb553e53039e3d3558d0bd
kuo220 <k0978337296@gmail.com>
2023-01-03 18:44:26 +0800

    Update BusinessHourForm

--- frontend/src/component/BusinessHourForm.js
+++ frontend/src/component/BusinessHourForm.js
@@ -1,132 +1,196 @@
 import * as React from 'react';
-import {InputNumber, Space} from 'antd';
+import dayjs from 'dayjs';
+import {InputNumber, Space, Row} from 'antd';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import TypedInputNumber from 'antd/es/input-number';
 
 function BusinessHourForm() {
+	const [value, setValue] = React.useState(dayjs('2014-08-18T21:11:54'));
+	const handleChange = (newValue) => {
+		setValue(newValue);
+	};
+
 	return (
 		<React.Fragment>
-			<Typography variant="h6" gutterBottom>
-				Business Hours
-			</Typography>
-			<Grid container spacing={1}>
-				<Grid item xs={12} md={6}>
-					<Typography variant="h5" gutterBottom>
-						Sunday
-					</Typography>
-				</Grid>
+			<LocalizationProvider dateAdapter={AdapterDayjs}>
+				<Typography variant="h6" gutterBottom>
+					Business Hours
+				</Typography>
 
-				<Grid item xs={12} md={6}>
-					<Space size="middle">
-							<InputNumber size="medium" min={1} max={24} defaultValue={1}/>
-							<div>
-								to
-							</div>
-							<InputNumber size="medium" min={1} max={24} defaultValue={24}/>	
-					</Space>
-				</Grid>
+				<Grid container spacing={2}>
+					<Grid item xs={12} md={6}>
+						<Typography variant="h5" gutterBottom>
+							Sunday
+						</Typography>
+					</Grid>
 
-				<Grid item xs={12} md={6}>
-					<Typography variant="h5" gutterBottom>
-						Monday
-					</Typography>
-				</Grid>
+					<Grid item xs={12} md={6}>
+						<Space size="middle">
+							<TimePicker
+								label="Time"
+								value={value}
+								onChange={handleChange}
+								renderInput={(params) => <TextField {...params} />}
+							/>
+									to
+							<TimePicker
+								label="Time"
+								value={value}
+								onChange={handleChange}
+								renderInput={(params) => <TextField {...params} />}
+							/>
+						</Space>
+					</Grid>
 
-				<Grid item xs={12} md={6}>
-					<Space size="middle">
-							<InputNumber size="medium" min={1} max={24} defaultValue={1}/>
-							<div>
-								to
-							</div>
-							<InputNumber size="medium" min={1} max={24} defaultValue={24}/>	
-					</Space>
-				</Grid>
+					<Grid item xs={12} md={6}>
+						<Typography variant="h5" gutterBottom>
+							Monday
+						</Typography>
+					</Grid>
 
-				<Grid item xs={12} md={6}>
-					<Typography variant="h5" gutterBottom>
-						Tuesday
-					</Typography>
-				</Grid>
+					<Grid item xs={12} md={6}>
+						<Space size="middle">
+							<TimePicker
+								label="Time"
+								value={value}
+								onChange={handleChange}
+								renderInput={(params) => <TextField {...params} />}
+							/>
+									to
+							<TimePicker
+								label="Time"
+								value={value}
+								onChange={handleChange}
+								renderInput={(params) => <TextField {...params} />}
+							/>
+						</Space>
+					</Grid>
 
-				<Grid item xs={12} md={6}>
-					<Space size="middle">
-							<InputNumber size="medium" min={1} max={24} defaultValue={1}/>
-							<div>
-								to
-							</div>
-							<InputNumber size="medium" min={1} max={24} defaultValue={24}/>	
-					</Space>
-				</Grid>
+					<Grid item xs={12} md={6}>
+						<Typography variant="h5" gutterBottom>
+							Tuesday
+						</Typography>
+					</Grid>
+					<Grid item xs={12} md={6}>
+						<Space size="middle">
+							<TimePicker
+								label="Time"
+								value={value}
+								onChange={handleChange}
+								renderInput={(params) => <TextField {...params} />}
+							/>
+									to
+							<TimePicker
+								label="Time"
+								value={value}
+								onChange={handleChange}
+								renderInput={(params) => <TextField {...params} />}
+							/>
+						</Space>
+					</Grid>
 
-				<Grid item xs={12} md={6}>
-					<Typography variant="h5" gutterBottom>
-						Wednesday
-					</Typography>
-				</Grid>
+					<Grid item xs={12} md={6}>
+						<Typography variant="h5" gutterBottom>
+							Wednesday
+						</Typography>
+					</Grid>
 
-				<Grid item xs={12} md={6}>
-					<Space size="middle">
-							<InputNumber size="medium" min={1} max={24} defaultValue={1}/>
-							<div>
-								to
-							</div>
-							<InputNumber size="medium" min={1} max={24} defaultValue={24}/>	
-					</Space>
-				</Grid>
+					<Grid item xs={12} md={6}>
+						<Space size="middle">
+							<TimePicker
+								label="Time"
+								value={value}
+								onChange={handleChange}
+								renderInput={(params) => <TextField {...params} />}
+							/>
+									to
+							<TimePicker
+								label="Time"
+								value={value}
+								onChange={handleChange}
+								renderInput={(params) => <TextField {...params} />}
+							/>
+						</Space>
+					</Grid>
 
-				<Grid item xs={12} md={6}>
-					<Typography variant="h5" gutterBottom>
-						Thursday
-					</Typography>
-				</Grid>
+					<Grid item xs={12} md={6}>
+						<Typography variant="h5" gutterBottom>
+							Thursday
+						</Typography>
+					</Grid>
 
-				<Grid item xs={12} md={6}>
-					<Space size="middle">
-							<InputNumber size="medium" min={1} max={24} defaultValue={1}/>
-							<div>
-								to
-							</div>
-							<InputNumber size="medium" min={1} max={24} defaultValue={24}/>	
-					</Space>
-				</Grid>
+					<Grid item xs={12} md={6}>
+						<Space size="middle">
+							<TimePicker
+								label="Time"
+								value={value}
+								onChange={handleChange}
+								renderInput={(params) => <TextField {...params} />}
+							/>
+									to
+							<TimePicker
+								label="Time"
+								value={value}
+								onChange={handleChange}
+								renderInput={(params) => <TextField {...params} />}
+							/>
+						</Space>
+					</Grid>
 
-				<Grid item xs={12} md={6}>
-					<Typography variant="h5" gutterBottom>
-						Friday
-					</Typography>
-				</Grid>
+					<Grid item xs={12} md={6}>
+						<Typography variant="h5" gutterBottom>
+							Friday
+						</Typography>
+					</Grid>
 
-				<Grid item xs={12} md={6}>
-					<Space size="middle">
-							<InputNumber size="medium" min={1} max={24} defaultValue={1}/>
-							<div>
-								to
-							</div>
-							<InputNumber size="medium" min={1} max={24} defaultValue={24}/>	
-					</Space>
-				</Grid>
+					<Grid item xs={12} md={6}>
+						<Space size="middle">
+							<TimePicker
+								label="Time"
+								value={value}
+								onChange={handleChange}
+								renderInput={(params) => <TextField {...params} />}
+							/>
+									to
+							<TimePicker
+								label="Time"
+								value={value}
+								onChange={handleChange}
+								renderInput={(params) => <TextField {...params} />}
+							/>
+						</Space>
+					</Grid>
 
-				<Grid item xs={12} md={6}>
-					<Typography variant="h5" gutterBottom>
-						Saturday
-					</Typography>
-				</Grid>
+					<Grid item xs={12} md={6}>
+						<Typography variant="h5" gutterBottom>
+							Saturday
+						</Typography>
+					</Grid>
 
-				<Grid item xs={12} md={6}>
-					<Space size="middle">
-							<InputNumber size="medium" min={1} max={24} defaultValue={1}/>
-							<div>
-								to
-							</div>
-							<InputNumber size="medium" min={1} max={24} defaultValue={24}/>	
-					</Space>
+					<Grid item xs={12} md={6}>
+						<Space size="middle">
+							<TimePicker
+								label="Time"
+								value={value}
+								onChange={handleChange}
+								renderInput={(params) => <TextField {...params} />}
+							/>
+									to
+							<TimePicker
+								label="Time"
+								value={value}
+								onChange={handleChange}
+								renderInput={(params) => <TextField {...params} />}
+							/>
+						</Space>
+					</Grid>
 				</Grid>
-
-			</Grid>
+			</LocalizationProvider>
+			
 		</React.Fragment>
 	);
 }
