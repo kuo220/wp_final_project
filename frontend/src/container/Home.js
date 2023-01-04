d39674fd523088584148d615f4198890cbf8f3c0
kuo220 <k0978337296@gmail.com>
2023-01-05 03:06:44 +0800

    update Home

--- frontend/src/container/Home.js
+++ frontend/src/container/Home.js
@@ -2,7 +2,12 @@ import { Typography } from 'antd';
 import Titlebar from './Titlebar';
 import styled from 'styled-components';
 import CssBaseline from '@mui/material/CssBaseline';
-import home from '../picture/home.jpg'
+import home2 from '../picture/home2.jpg'
+import Divider from '@mui/material/Divider';
+import Button from '@mui/material/Button';
+import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+
 
 const HomeBackground = styled.div`
 	width : 100vw;
@@ -11,11 +16,43 @@ const HomeBackground = styled.div`
 `
 
 function Home() {
-	return(
+    const navigate = useNavigate();
+
+    const LoginButton = {
+        position : 'absolute',
+        width : '8%',
+        height : '6%',
+        top : '5%',
+        right : '5%',
+        backgroundColor : 'gray',
         
+    }
+
+    const SignUpButton = {
+        position : 'absolute',
+        width : '8%',
+        height : '6%',
+        top : '5%',
+        right : '15%',
+        backgroundColor : 'gray'
+    
+    }
+
+	return(
         <HomeBackground>
             <CssBaseline/>
-            <img src={home} style={{width:'100%', height:'100%'}}/>
+            <Button variant="contained" size='large' color="primary" style={SignUpButton}
+                onClick={() => {navigate('/register')}}
+            > Sign Up </Button>
+            <Button variant="contained" size='large' color="primary" style={LoginButton}
+                onClick={() => {navigate('/login')}}
+            > Log In </Button>
+
+            <img src={home2} style={{width:'100%', height:'100%'}}>
+                
+            </img>
+
+            
         </HomeBackground>
     )
 }
